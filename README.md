9f75fac940331597c034da9530c8c8010b5dbc12
Lucas Zanek <lucaszanek94@gmail.com>
2023-04-06 01:26:04 -0300

    Added core functionallity tests on Rust file main

--- README.md
+++ README.md
@@ -60,8 +60,6 @@ The following are some important warnings that you should be aware of before mod
 
 Modify at your own risk: This code generates a private key and a mnemonic phrase based on a username, password, and a PIN. Modifying any part of this code may affect the generation process, and result in loss of access to your funds. It is recommended that you do not modify this code unless you fully understand its implications.
 Not audited for security: This code has not been audited for security, and its use is at your own risk. It is recommended that you do not use this code with significant amounts of funds.
-Unit tests not yet added: At the moment, this code does not have any unit tests to verify the deterministic generation of private keys and mnemonic phrases. It is recommended that you add unit tests to verify the correctness of the generation process.
-Not meant for production: This application is not meant to be used in a production environment, and is only intended for educational purposes. It is recommended that you use a trusted wallet provider for storing your funds.
 
 Styles
 The styles for this app are a work in progress, and will be updated in the future.
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -27,6 +27,97 @@ fn private_key_to_mnemonic(private_key: &H256) -> String {
     mnemonic.to_string()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_different_inputs_produce_different_keys_and_mnemonics() {
+        let input1 = ("user1".to_string(), "pass1".to_string(), "1234".to_string());
+        let input2 = ("user2".to_string(), "pass2".to_string(), "5678".to_string());
+
+        let (private_key1, mnemonic1) = generate_values(input1.0.clone(), input1.1.clone(), input1.2.clone());
+        let (private_key2, mnemonic2) = generate_values(input2.0.clone(), input2.1.clone(), input2.2.clone());
+
+        assert_ne!(private_key1, private_key2);
+        assert_ne!(mnemonic1, mnemonic2);
+    }
+
+    #[test]
+    fn test_same_input_produces_same_key_and_mnemonic() {
+        let input = ("user1".to_string(), "pass1".to_string(), "1234".to_string());
+
+        let (private_key1, mnemonic1) = generate_values(input.0.clone(), input.1.clone(), input.2.clone());
+        let (private_key2, mnemonic2) = generate_values(input.0.clone(), input.1.clone(), input.2.clone());
+
+        assert_eq!(private_key1, private_key2);
+        assert_eq!(mnemonic1, mnemonic2);
+    }
+
+    #[test]
+    fn test_special_characters_produce_different_keys_and_mnemonics() {
+        let input1 = ("user#1".to_string(), "pass$1".to_string(), "@1234".to_string());
+        let input2 = ("user#2".to_string(), "pass$2".to_string(), "@5678".to_string());
+
+        let (private_key1, mnemonic1) = generate_values(input1.0.clone(), input1.1.clone(), input1.2.clone());
+        let (private_key2, mnemonic2) = generate_values(input2.0.clone(), input2.1.clone(), input2.2.clone());
+
+        assert_ne!(private_key1, private_key2);
+        assert_ne!(mnemonic1, mnemonic2);
+    }
+
+    #[test]
+    fn test_case_sensitive_different_keys_and_mnemonics() {
+        let input1 = ("housE".to_string(), "Pass1".to_string(), "1234".to_string());
+        let input2 = ("house".to_string(), "pass1".to_string(), "1234".to_string());
+
+        let (private_key1, mnemonic1) = generate_values(input1.0.clone(), input1.1.clone(), input1.2.clone());
+        let (private_key2, mnemonic2) = generate_values(input2.0.clone(), input2.1.clone(), input2.2.clone());
+
+        assert_ne!(private_key1, private_key2);
+        assert_ne!(mnemonic1, mnemonic2);
+    }
+
+    #[test]
+    fn test_case_sensitive_same_keys_and_mnemonics() {
+        let input = ("housE".to_string(), "Pass1".to_string(), "1234".to_string());
+
+        let (private_key1, mnemonic1) = generate_values(input.0.clone(), input.1.clone(), input.2.clone());
+        let (private_key2, mnemonic2) = generate_values(input.0.clone(), input.1.clone(), input.2.clone());
+
+        assert_eq!(private_key1, private_key2);
+        assert_eq!(mnemonic1, mnemonic2);
+    }
+
+    #[test]
+    fn test_empty_strings_produce_valid_keys_and_mnemonics() {
+        let input = ("".to_string(), "".to_string(), "".to_string());
+
+        let (private_key1, mnemonic1) = generate_values(input.0.clone(), input.1.clone(), input.2.clone());
+        let (private_key2, mnemonic2) = generate_values(input.0.clone(), input.1.clone(), input.2.clone());
+
+        assert_eq!(private_key1, private_key2);
+        assert_eq!(mnemonic1, mnemonic2);
+        assert_ne!(private_key1, "".to_string());
+        assert_ne!(mnemonic1, "".to_string());
+    }
+
+    #[test]
+    fn test_unicode_characters_produce_different_keys_and_mnemonics() {
+        let input1 = ("😃🏠".to_string(), "🔑1".to_string(), "1🐶34".to_string());
+        let input2 = ("😺🏠".to_string(), "🔑2".to_string(), "5🐱78".to_string());
+
+        let (private_key1, mnemonic1) = generate_values(input1.0.clone(), input1.1.clone(), input1.2.clone());
+        let (private_key2, mnemonic2) = generate_values(input2.0.clone(), input2.1.clone(), input2.2.clone());
+
+        assert_ne!(private_key1, private_key2);
+        assert_ne!(mnemonic1, mnemonic2);
+    }
+    
+
+}
+
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![generate_values])
