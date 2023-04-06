fbd550a095fd70884154cb5b2b277b69ad9cffd3
Lucas Zanek <lucaszanek94@gmail.com>
2023-04-06 15:22:46 -0300

    fixed on submit

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,23 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import GradientText from "./GradientText";
 
+interface Keys {
+  privateKey: string;
+  mnemonic: string;
+}
 function App() {
-  const [output, setOutput] = useState<{
-    privateKey: string;
-    mnemonic: string;
-  }>({ privateKey: "", mnemonic: "" });
+  const [output, setOutput] = useState<Keys>({
+    privateKey: "",
+    mnemonic: "",
+  });
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [pin, setPin] = useState("");
   const [showPrivateKey, setShowPrivateKey] = useState(false);
   const [showMnemonic, setShowMnemonic] = useState(false);
   const [keyboard, setKeyboard] = useState<number[]>([]);
-
-  const [gradientIndex, setGradientIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setGradientIndex((prevIndex) => (prevIndex + 1) % 3);
-    }, 8000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPin, setShowPin] = useState(false);
 
   async function generateKeys() {
     if (validateFields()) {
@@ -87,122 +82,139 @@ function App() {
         <GradientText />
 
         <div className="form-container">
-          <div className="form-container">
-            <form
-              className="form"
-              onSubmit={(e) => {
-                e.preventDefault();
-                generateKeys();
-              }}
-              onKeyDown={handleKeyPress}
+          <form
+            className="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            onKeyDown={handleKeyPress}
+          >
+            <label className="label" htmlFor="username-input">
+              Username:
+            </label>
+            <div className="input-container">
+              <input
+                className="input"
+                id="username-input"
+                value={username}
+                onChange={(e) =>
+                  setUsername(e.currentTarget.value.toLowerCase())
+                }
+                placeholder="Enter username..."
+              />
+            </div>
+            <label className="label" htmlFor="password-input">
+              Password:
+            </label>
+            <div className="input-container">
+              <input
+                className="input"
+                id="password-input"
+                value={password}
+                onChange={(e) =>
+                  setPassword(e.currentTarget.value.toLowerCase())
+                }
+                placeholder="Enter password..."
+                type={showPin ? "text" : "password"} // mostrar/ocultar pin
+              />
+            </div>
+            <label className="label" htmlFor="pin-input">
+              PIN
+            </label>
+            <input
+              id="pin-input"
+              value={pin}
+              readOnly
+              type={showPin ? "text" : "password"} // mostrar/ocultar pin
+            />
+            <button
+              type="button"
+              onClick={() => setShowPin(!showPin)} // alternar estado
             >
-              <div className="input-container">
-                <label className="label" htmlFor="username-input">
-                  Username:
-                </label>
-                <input
-                  className="input"
-                  id="username-input"
-                  value={username}
-                  onChange={(e) => setUsername(e.currentTarget.value)}
-                  placeholder="Enter username..."
-                />
-              </div>
-              <div className="input-container">
-                <label className="label" htmlFor="password-input">
-                  Password:
-                </label>
-                <input
-                  className="input"
-                  id="password-input"
-                  value={password}
-                  onChange={(e) => setPassword(e.currentTarget.value)}
-                  placeholder="Enter password..."
-                  type="password"
-                />
-              </div>
+              {showPin ? "Hide" : "Show"}
+            </button>
 
-              <div className="input-container keyboard-container">
-                <label className="label" htmlFor="pin-input">
-                  PIN:
-                </label>
-                <input id="pin-input" value={pin} readOnly />
+            <div className="keyboard">
+              <div className="keyboard-row">
+                {keyboard.slice(0, 3).map((number) => (
+                  <button
+                    key={number}
+                    className="keyboard-button"
+                    onClick={() => handleKeyClick(number)}
+                  >
+                    {number}
+                  </button>
+                ))}
               </div>
-              <div className="keyboard">
-                <div className="keyboard-row">
-                  {keyboard.slice(0, 3).map((number) => (
-                    <button
-                      key={number}
-                      className="keyboard-button"
-                      onClick={() => handleKeyClick(number)}
-                    >
-                      {number}
-                    </button>
-                  ))}
-                </div>
-                <div className="keyboard-row">
-                  {keyboard.slice(3, 6).map((number) => (
-                    <button
-                      key={number}
-                      className="keyboard-button"
-                      onClick={() => handleKeyClick(number)}
-                    >
-                      {number}
-                    </button>
-                  ))}
-                </div>
-                <div className="keyboard-row">
-                  {keyboard.slice(6, 10).map((number) => (
-                    <button
-                      key={number}
-                      className="keyboard-button"
-                      onClick={() => handleKeyClick(number)}
-                    >
-                      {number}
-                    </button>
-                  ))}
-                </div>
-                <button>Generate</button>
+              <div className="keyboard-row">
+                {keyboard.slice(3, 6).map((number) => (
+                  <button
+                    key={number}
+                    className="keyboard-button"
+                    onClick={() => handleKeyClick(number)}
+                  >
+                    {number}
+                  </button>
+                ))}
               </div>
-            </form>
-            <div className="output">
-              <div className="output-row">
-                <label htmlFor="private-key">Private Key:</label>
-                <input
-                  id="private-key"
-                  value={
-                    showPrivateKey
-                      ? output.privateKey
-                      : "•".repeat(output.privateKey.length)
-                  }
-                  readOnly
-                />
-                <button onClick={() => setShowPrivateKey(!showPrivateKey)}>
-                  {showPrivateKey ? "Hide" : "Show"}
-                </button>
-                <button onClick={() => copyToClipboard(output.privateKey)}>
-                  Copy
-                </button>
+              <div className="keyboard-row">
+                {keyboard.slice(6, 10).map((number) => (
+                  <button
+                    key={number}
+                    className="keyboard-button"
+                    onClick={() => handleKeyClick(number)}
+                  >
+                    {number}
+                  </button>
+                ))}
               </div>
-              <div className="output-row">
-                <label htmlFor="mnemonic">Mnemonic Phrase:</label>
-                <input
-                  id="mnemonic"
-                  value={
-                    showMnemonic
-                      ? output.mnemonic
-                      : "•".repeat(output.mnemonic.length)
-                  }
-                  readOnly
-                />
-                <button onClick={() => setShowMnemonic(!showMnemonic)}>
-                  {showMnemonic ? "Hide" : "Show"}
-                </button>
-                <button onClick={() => copyToClipboard(output.mnemonic)}>
-                  Copy
-                </button>
+              <button type="button" onClick={generateKeys}>
+                Generate
+              </button>
+            </div>
+          </form>
+          <div className="output-row">
+            <label htmlFor="private-key">Private Key:</label>
+            <input
+              id="private-key"
+              value={
+                showPrivateKey
+                  ? output.privateKey
+                  : "•".repeat(output.privateKey.length)
+              }
+              readOnly
+            />
+            <div className="button-container">
+              <button onClick={() => setShowPrivateKey(!showPrivateKey)}>
+                {showPrivateKey ? "Hide" : "Show"}
+              </button>
+              <button onClick={() => copyToClipboard(output.privateKey)}>
+                Copy
+              </button>
+            </div>
+
+            <div className="output-row">
+              <label htmlFor="mnemonic">Mnemonic Phrase:</label>
+              <input
+                id="mnemonic"
+                value={
+                  showMnemonic
+                    ? output.mnemonic
+                    : "•".repeat(output.mnemonic.length)
+                }
+                readOnly
+              />
+            </div>
+            <div className="button-container">
+              <button onClick={() => setShowMnemonic(!showMnemonic)}>
+                {showMnemonic ? "Hide" : "Show"}
+              </button>
+              <button onClick={() => copyToClipboard(output.mnemonic)}>
+                Copy
+              </button>
+              <div>
+                <button onClick={cleanFields}>Clean</button>
               </div>
-              <button onClick={cleanFields}>Clean</button>
             </div>
           </div>
         </div>
